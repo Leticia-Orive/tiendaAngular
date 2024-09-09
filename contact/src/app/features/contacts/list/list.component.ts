2d15088729e40b9550064af25c9db7d14cf7a629
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-09-09 08:57:33 +0200

    subscribirte en los observables con takeUntilDestroyed

--- contact/src/app/features/contacts/list/list.component.ts
+++ contact/src/app/features/contacts/list/list.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { GridComponent } from '@components/grid/grid.component';
 import { ColumnKeys, Contact } from '../contact.interfaces';
 import { ContactService } from '../contact.service';
 import { tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list',
@@ -25,6 +26,7 @@ export class ListComponent implements OnInit {
   sortables: ColumnKeys<Contact> = ['id', 'name', 'phone', 'email'];
 
   private readonly _contactSvc = inject(ContactService);
+  private readonly _destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     this.getAllContacts();
@@ -33,6 +35,7 @@ export class ListComponent implements OnInit {
   getAllContacts(){
     this._contactSvc.getAllContacts()
     .pipe(
+      takeUntilDestroyed(this._destroyRef),
       tap((contacts:Contact[]) => this.data = [...contacts])
     )
     .subscribe() 
@@ -42,3 +45,7 @@ export class ListComponent implements OnInit {
   
 
 }
+function takeUntilDestroy(_destroy$: any): import("rxjs").OperatorFunction<Contact[], Contact[]> {
+  throw new Error('Function not implemented.');
+}
+
