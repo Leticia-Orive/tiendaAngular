e0242ac2df4e2a03868a5eba17b59203a68284b3
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-09-12 09:30:59 +0200

    intalacion y configuracion de firebase

--- contacts/src/app/app.config.ts
+++ contacts/src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {environment} from '../environments/environment';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), provideAnimationsAsync(), 
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"crud-contact-b082f","appId":"1:106134067093:web:40b66a352b71591791c89c","storageBucket":"crud-contact-b082f.appspot.com","apiKey":"AIzaSyAEwHiEtTTcLg6bG_7CO1GMX-phl2GWlZg","authDomain":"crud-contact-b082f.firebaseapp.com","messagingSenderId":"106134067093","measurementId":"G-XVY8QSJKYP"})), provideFirestore(() => getFirestore())]
 };
