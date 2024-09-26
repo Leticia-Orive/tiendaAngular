54a965e285e10b85222c4a9122f111e1d3be7772
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-09-26 15:27:01 +0200

    Implementar la lógica de componentes

--- ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product.model';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+  products: Product[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  deleteProduct(id: number): void {
+    this.apiService.deleteProduct(id).subscribe(() => {
+      this.products = this.products.filter((product) => product.id !== id);
+    });
+  }
 }
