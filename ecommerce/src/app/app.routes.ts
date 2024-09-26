7824a08c8563e545f4cbb7f0626d7fa5586754f7
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-09-26 15:28:48 +0200

    Configurar el enrutamiento

--- ecommerce/src/app/app.routes.ts
+++ ecommerce/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'products', component: ProductListComponent },
+  { path: 'add-product', component: ProductAddComponent },
+];
