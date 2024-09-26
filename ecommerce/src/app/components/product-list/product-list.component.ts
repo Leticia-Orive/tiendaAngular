39498b32f19bac1dadf467a06380cc442ddadc91
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-09-26 15:38:52 +0200

    arreglar para que salgan los metodos

--- ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../interfaces/product.model';
 import { ApiService } from '../../services/api.service';
 import { MaterialModule } from '../../models/materialModule';
+import { Router } from '@angular/router'; // Importa Router si quieres redirigir
 
 @Component({
   selector: 'app-product-list',
@@ -13,14 +14,31 @@ import { MaterialModule } from '../../models/materialModule';
 export class ProductListComponent implements OnInit{
   products: Product[] = [];
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit(): void {
     this.apiService.getProducts().subscribe((data) => {
       this.products = data;
     });
   }
+  viewProduct(id: number): void {
+    // Por ejemplo, redirigir a la página de detalles del producto
+    this.router.navigate(['/product', id]);
+    
+    // O si estás usando un modal, puedes abrirlo aquí
+    // this.openProductModal(id);
+  }
 
+  // Si decides usar un modal, puedes implementar un método como este:
+  // openProductModal(id: number): void {
+  //   // Lógica para abrir un modal
+  // }
+
+  // Agrega el método editProduct
+  editProduct(id: number): void {
+    // Redirigir a la página de edición del producto
+    this.router.navigate(['/edit-product', id]);
+  }
   deleteProduct(id: number): void {
     this.apiService.deleteProduct(id).subscribe(() => {
       this.products = this.products.filter((product) => product.id !== id);
