b249f8c3157ef2d25502259d0a5575e3ecdbeb88
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-09-26 15:39:12 +0200

    añadir rutas

--- ecommerce/src/app/app.routes.ts
+++ ecommerce/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import { ProductViewComponent } from './components/product-view/product-view.component';
 
 export const routes: Routes = [
     { path: 'products', component: ProductListComponent },
   { path: 'add-product', component: ProductAddComponent },
+  { path: 'product/:id', component: ProductViewComponent }, // Ruta para ver el producto
+  { path: 'edit-product/:id', component: ProductEditComponent }, // Ruta para editar el producto
 ];
